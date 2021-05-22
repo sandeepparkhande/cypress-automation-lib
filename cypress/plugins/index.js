bc8a46fdb9a2acc3beb8fa9f6f8db2e5dde9d1b1
Sandeep Parkhande <sandeep.parkhande@gmail.com>
2021-05-22 17:09:14 +0530

    Removed web pack suport

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,21 +1,12 @@
-// ***********************************************************
-// This example plugins/index.js can be used to load plugins
-//
-// You can change the location of this file or turn off loading
-// the plugins file with the 'pluginsFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/plugins-guide
-// ***********************************************************
+const browserify = require('@cypress/browserify-preprocessor');
+const cucumber = require('cypress-cucumber-preprocessor').default;
+const resolve = require('resolve');
 
-// This function is called when a project is opened or re-opened (e.g. due to
-// the project's config changing)
-
-const webpack = require("@cypress/webpack-preprocessor");
-
-module.exports = on => {
+module.exports = (on, config) => {
     const options = {
-        webpackOptions: require("../webpack.config.js")
+        ...browserify.defaultOptions,
+        typescript: resolve.sync('typescript', { baseDir: config.projectRoot }),
     };
-    on("file:preprocessor", webpack(options));
+
+    on('file:preprocessor', cucumber(options));
 };
